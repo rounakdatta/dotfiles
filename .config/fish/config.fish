2c808fb44b409b30cc1f2f8933aee44115903622
Rounak Datta <rounakdatta12@gmail.com>
2020-09-19 15:18:02 +0530

    add missing tmux-yank

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if [ -n "$TMUX_PANE" ]
   set HISTFILE $HOME/.local/share/fish/fish_history_tmux_$TMUX_PANE
 end
 
+# set java home
+set JAVA_HOME /usr/libexec/java_home
+
 # set local hive and remote connections through beeline correct
 set HADOOP_HOME ~/tooling/hadoop-2.5.1
 set HIVE_HOME ~/tooling/apache-hive-1.2.1-bin
