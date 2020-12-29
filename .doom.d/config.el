3e5f4a6cfdb240e4b054e86b05b751ff73e96bbf
Rounak Datta <rounakdatta12@gmail.com>
2020-12-29 23:19:34 +0530

    add companion roam modes; set org-directory for plain org mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/personal/rounakdatta.github.io/")
 (setq org-roam-directory "~/personal/rounakdatta.github.io")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
