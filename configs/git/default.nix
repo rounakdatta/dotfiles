4282bf3d38de458dda51fbdc9c1dd259c227e3d1
Rounak Datta <rounak@lyric.tech>
2024-11-29 17:40:12 +0530

    Fix custom git config for work

--- configs/git/default.nix
+++ configs/git/default.nix
@@ -7,11 +7,13 @@
       key = "A04E86FD28F5A421";
       signByDefault = true;
     };
+    # the goal here is to have the correct ordering, the `[user]` block should come first, and then the `[include]` block
+    # was able to fix the ordering using https://www.reddit.com/r/NixOS/comments/jg4i92/comment/j08vf4n
+    includes = [
+      { path = "~/.gitconfig.work"; }
+    ];
     extraConfig = {
       diff.external = "difft";
-      "includeIf \"gitdir:~/work/\"" = {
-        path = "~/.gitconfig.work";
-      };
     };
   };
 
