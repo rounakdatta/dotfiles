051af4283314b60ac7797cef241e2102135ab1b2
Rounak Datta <rounakdatta12@gmail.com>
2025-10-17 16:00:49 +0530

    Setup claude code integration on nvim

--- configs/vim/default.nix
+++ configs/vim/default.nix
@@ -2,6 +2,39 @@
   programs.neovim = {
     enable = true;
     vimAlias = true;
+    
+    plugins = with pkgs.vimPlugins; [
+      plenary-nvim
+      {
+        plugin = pkgs.vimUtils.buildVimPlugin {
+          name = "claude-code.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "greggh";
+            repo = "claude-code.nvim";
+            rev = "main";
+            sha256 = "sha256-0crfj852lwif5gipckb3hzagrvjccl6jg7xghs02d0v1vjx0yhk4";
+          };
+        };
+        type = "lua";
+        config = ''
+          require("claude-code").setup({
+            -- Optional: Configure window settings
+            window = {
+              relative = "editor",
+              width = 0.8,
+              height = 0.8,
+              border = "rounded",
+            },
+          })
+          
+          -- Key mappings
+          vim.keymap.set('n', '<C-,>', ':ClaudeCode<CR>', { noremap = true, silent = true })
+          vim.keymap.set('n', '<leader>cC', ':ClaudeCodeContinue<CR>', { noremap = true, silent = true })
+          vim.keymap.set('n', '<leader>cV', ':ClaudeCodeVerbose<CR>', { noremap = true, silent = true })
+        '';
+      }
+    ];
+    
     extraConfig = ''
       set clipboard=unnamedplus
       set autoindent
