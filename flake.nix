463b80daabf4433c5f7c99363eb2ff4a8af401c7
Rounak Datta <rounakdatta12@gmail.com>
2025-11-19 11:19:38 +0530

    Fix: Pass user attribute to home-manager modules

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
             home-manager.users.${user.username} = {
               imports = [ ./hosts/ninezeroes/home.nix ];
             };
