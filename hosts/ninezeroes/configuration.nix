5a40662ec784dff48e0d5335aafa307229909ae1
Rounak Datta <rounakdatta12@gmail.com>
2025-12-12 21:21:50 +0530

    Fix errors from ninezeroes

--- hosts/ninezeroes/configuration.nix
+++ hosts/ninezeroes/configuration.nix
@@ -66,8 +66,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -81,7 +81,7 @@
   services.pcscd.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
